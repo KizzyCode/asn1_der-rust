0f042778234a747e47803fa26e167baca466209a
Keziah Biermann <development@kizzycode.de>
2023-04-06 04:25:19 +0200

    Disable no-panic tests

--- test_unix.sh
+++ test_unix.sh
@@ -5,10 +5,10 @@ cargo clean && cargo test --verbose --release --no-default-features
 
 cargo test --verbose --release --no-default-features --features="native_types"
 cargo test --verbose --release --no-default-features --features="std"
-cargo clean && cargo test --verbose --release --no-default-features --features="no_panic"
+#cargo clean && cargo test --verbose --release --no-default-features --features="no_panic"
 
 cargo test --verbose --release --no-default-features --features="native_types,std"
-cargo clean && cargo test --verbose --release --no-default-features --features="std,no_panic"
-cargo clean && cargo test --verbose --release --no-default-features --features="no_panic,native_types"
+#cargo clean && cargo test --verbose --release --no-default-features --features="std,no_panic"
+#cargo clean && cargo test --verbose --release --no-default-features --features="no_panic,native_types"
 
-cargo clean && cargo test --verbose --release --no-default-features --features="native_types,std,no_panic"
+#cargo clean && cargo test --verbose --release --no-default-features --features="native_types,std,no_panic"
